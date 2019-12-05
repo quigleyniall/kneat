6371d7e4daa833e6f8aba7d38c6ac363ab0021fb
Niall Quigley <niallquigley92@gmail.com>
2019-12-05 15:49:10 +0000

    Set up linting

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'none',
+  singleQuote: true,
+  tabWidth: 2,
+  newlineBeforeReturn: true,
+  noDuplicateVariable: [true, 'check-parameters']
+};
