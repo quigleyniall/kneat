5aa8e11f429fc634fd1a0422b60034b54b898958
Niall Quigley <niallquigley92@gmail.com>
2019-12-05 19:51:46 +0000

    Unstyled Search bar

--- client/src/components/SearchBar/SearchBar.tsx
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -3,9 +3,14 @@ import React from 'react';
 interface SearchBarProps {
   onSearchChange: (e) => void;
   onPress: () => void;
+  setNoResults: () => void;
 }
 
-const SearchBar = ({ onSearchChange, onPress }: SearchBarProps) => (
+const SearchBar = ({
+  onSearchChange,
+  onPress,
+  setNoResults
+}: SearchBarProps) => (
   <div data-test="search-bar">
     <input
       name="searchBar"
@@ -14,6 +19,7 @@ const SearchBar = ({ onSearchChange, onPress }: SearchBarProps) => (
       onChange={onSearchChange}
     />
     <button onClick={onPress}>Search</button>
+    <button onClick={setNoResults}>Result per page</button>
   </div>
 );
 
