5dfaf0e28d01e242aa8401bff87dff257fa6ccb3
Niall Quigley <niallquigley92@gmail.com>
2019-12-05 19:52:08 +0000

    Basic Result output

--- client/src/components/Results/Results.tsx
+++ client/src/components/Results/Results.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import './Results.scss';
 
-const Results = () => <div>These are the results</div>;
+interface Props {
+  starShip: any;
+}
+
+const Results = ({ starShip }: Props) => (
+  <div className="result">
+    <div>Name: {starShip.name}</div>
+    <div>Model: {starShip.model}</div>
+    <div>MGLT: {starShip.MGLT}</div>
+    <div>Consumables: {starShip.consumables}</div>
+    <div>
+      {starShip.number_of_resupplies
+        ? `Number of Resupplies: ${starShip.number_of_resupplies}`
+        : null}
+    </div>
+  </div>
+);
 
 export default Results;
