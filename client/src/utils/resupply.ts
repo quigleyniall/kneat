3c2a4ba2e46c2c1c48d004f6a7af850f9cff103c
Niall Quigley <niallquigley92@gmail.com>
2019-12-05 19:50:49 +0000

    Resupply helper function

---
+++ client/src/utils/resupply.ts
@@ -0,0 +1,23 @@
+export const checkResupply = consumable => {
+  const timeArray = consumable.split(' ');
+  const timeString = timeArray[1];
+  let hoursToResupply;
+
+  if (timeString.includes('hour')) {
+    hoursToResupply = 1;
+  }
+  if (timeString.includes('day')) {
+    hoursToResupply = 24;
+  }
+  if (timeString.includes('week')) {
+    hoursToResupply = 24 * 7;
+  }
+  if (timeString.includes('month')) {
+    hoursToResupply = 24 * 30;
+  }
+  if (timeString.includes('year')) {
+    hoursToResupply = 24 * 365;
+  }
+
+  return hoursToResupply * +timeArray[0];
+};
