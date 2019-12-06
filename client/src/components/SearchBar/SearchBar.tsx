e446b5cea4f7d204661072c315e7f4ba3c02b253
Niall Quigley <niallquigley92@gmail.com>
2019-12-06 23:59:04 +0000

    Search Bar

--- client/src/components/SearchBar/SearchBar.tsx
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
+import Button from '../Button/Button';
+import TextInput from '../TextInput';
+import './SearchBar.scss';
 
 interface SearchBarProps {
   onSearchChange: (e) => void;
   onPress: () => void;
-  setNoResults: () => void;
+  searching: boolean;
+  searchTerm: string;
 }
 
 const SearchBar = ({
   onSearchChange,
   onPress,
-  setNoResults
-}: SearchBarProps) => (
-  <div data-test="search-bar">
-    <input
-      name="searchBar"
-      className="search"
-      type="text"
-      onChange={onSearchChange}
-    />
-    <button onClick={onPress}>Search</button>
-    <button onClick={setNoResults}>Result per page</button>
-  </div>
-);
+  searching,
+  searchTerm
+}: SearchBarProps) => {
+  const btnClass = searching || searchTerm.length === 0 ? 'disabled' : 'search';
+  return (
+    <div className="flex">
+      <TextInput name="searchBar" type="text" handleChange={onSearchChange} />
+      <Button
+        text="Calculate Number of Re-Supplies"
+        onPress={onPress}
+        btnClass={btnClass}
+        disabled={searching || searchTerm.length === 0}
+      />
+    </div>
+  );
+};
 
 export default SearchBar;
