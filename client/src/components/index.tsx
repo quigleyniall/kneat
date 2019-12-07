bfc0632aa0404871fba1e6bfcca71a1b673cc2c4
Niall Quigley <niallquigley92@gmail.com>
2019-12-07 00:00:39 +0000

    Export components

---
+++ client/src/components/index.tsx
@@ -0,0 +1,23 @@
+import Button from './Button';
+import Checkbox from './Checkbox';
+import Dropdown from './Dropdown';
+import Heading from './Heading';
+import Page from './Page';
+import SearchBar from './SearchBar';
+import AddRemoveFromTable from './AddRemoveFromTable';
+import TableRow from './TableRow';
+import TableWrapper from './TableWrapper';
+import TextInput from './TextInput';
+
+export {
+  Button,
+  Checkbox,
+  Dropdown,
+  Heading,
+  Page,
+  SearchBar,
+  AddRemoveFromTable,
+  TableRow,
+  TableWrapper,
+  TextInput
+};
