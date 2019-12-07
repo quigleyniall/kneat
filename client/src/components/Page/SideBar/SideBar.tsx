d9056966c5b142bad48b37c50d9d15437971844b
Niall Quigley <niallquigley92@gmail.com>
2019-12-07 18:53:33 +0000

    Remove unneccesary {}

--- client/src/components/Page/SideBar/SideBar.tsx
+++ client/src/components/Page/SideBar/SideBar.tsx
@@ -2,24 +2,22 @@ import React from 'react';
 import kneat from '../../../assets/kneat-logo.svg';
 import './SideBar.scss';
 
-const Sidemenu = ({ analysisLinks }: { analysisLinks: string[] }) => {
-  return (
-    <div className="sidebar">
-      <div className="top">
-        <div className="sidebar-header">Analysis</div>
-        <ul className="menu">
-          {analysisLinks.map((link, i) => {
-            return (
-              <li key={i} onClick={() => {}} className="link">
-                {link}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-      <img src={kneat} alt="logo" className="logo" />
+const Sidemenu = ({ analysisLinks }: { analysisLinks: string[] }) => (
+  <div className="sidebar">
+    <div className="top">
+      <div className="sidebar-header">Analysis</div>
+      <ul className="menu">
+        {analysisLinks.map((link, i) => {
+          return (
+            <li key={i} className="link">
+              {link}
+            </li>
+          );
+        })}
+      </ul>
     </div>
-  );
-};
+    <img src={kneat} alt="logo" className="logo" />
+  </div>
+);
 
 export default Sidemenu;
