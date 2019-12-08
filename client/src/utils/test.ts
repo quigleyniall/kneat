3a7e3e27232ece4cf27b65a1128f628235a4d577
Niall Quigley <niallquigley92@gmail.com>
2019-12-08 17:45:39 +0000

    check prop types helper function

--- client/src/utils/test.ts
+++ client/src/utils/test.ts
@@ -1,3 +1,16 @@
-export const findByTestAttr = (wrapper, dataTestId) => {
+import checkPropTypes from 'check-prop-types';
+
+export const findByTestAttr = (wrapper: any, dataTestId: string) => {
   return wrapper.find(`[data-test="${dataTestId}"]`);
+};
+
+export const checkProps = (component: any, conformingProps: any) => {
+  const propError = checkPropTypes(
+    component.propTypes,
+    conformingProps,
+    'prop',
+    component.name
+  );
+  // eslint-disable-next-line no-undef
+  expect(propError).toBeUndefined();
 };
