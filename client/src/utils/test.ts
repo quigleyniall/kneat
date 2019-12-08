57c7d2d0b6f6df36bc2551a195d9fb424fa22b68
Niall Quigley <niallquigley92@gmail.com>
2019-12-08 21:49:12 +0000

    Fix linting

--- client/src/utils/test.ts
+++ client/src/utils/test.ts
@@ -1,9 +1,11 @@
 import { checkPropTypes } from 'prop-types';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const findByTestAttr = (wrapper: any, dataTestId: string) => {
   return wrapper.find(`[data-test="${dataTestId}"]`);
 };
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const checkProps = (component: any, conformingProps: any) => {
   const propError = checkPropTypes(
     component.propTypes,
