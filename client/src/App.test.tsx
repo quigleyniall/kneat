1d38655d4ce84493dfb681b24f8f3c3b50a2121f
Niall Quigley <niallquigley92@gmail.com>
2019-12-09 17:35:44 +0000

    Unit test app

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,16 +1,177 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from './App';
-import { findByTestAttr } from './utils/test';
+import App, { UnconnectedApp } from './App';
+import { storeFactory, findByTestAttr } from './utils/test';
+import { initialState } from './store/reducers/starShip';
+import { sampleResponse } from './utils/sampleResponse';
+import { calcNumResupplies } from './store/actions';
+import { sortAlphabetically } from './utils/sorting';
 
-describe('Home Page', () => {
-  let wrapper: React.ReactNode;
+describe('redux properties', () => {
+  const setup = (state = {}) => {
+    const store = storeFactory(state);
+    const wrapper = shallow(<App store={store} />)
+      .dive()
+      .dive();
+    return wrapper;
+  };
+  test('has access to action creators', () => {
+    const wrapper = setup();
+    const makeApiCallProp = wrapper.instance().props.makeApiCall;
+    const changeTableHeadersProp = wrapper.instance().props.changeTableHeaders;
+    const calcNumResuppliesProp = wrapper.instance().props.calcNumResupplies;
+    const sortAlphabeticallyProp = wrapper.instance().props.sortAlphabetically;
+    const sortNummericallyProp = wrapper.instance().props.sortNummerically;
+    const sortConsumablesProp = wrapper.instance().props.sortConsumables;
+    const reverseSortProp = wrapper.instance().props.reverseSort;
+    const onSearchChangeProp = wrapper.instance().props.onSearchChange;
+    const setLoadingProp = wrapper.instance().props.setLoading;
 
-  // beforeEach(() => {
-  //   wrapper = shallow(<App />);
-  // });
+    expect(makeApiCallProp).toBeInstanceOf(Function);
+    expect(changeTableHeadersProp).toBeInstanceOf(Function);
+    expect(calcNumResuppliesProp).toBeInstanceOf(Function);
+    expect(sortAlphabeticallyProp).toBeInstanceOf(Function);
+    expect(sortNummericallyProp).toBeInstanceOf(Function);
+    expect(sortConsumablesProp).toBeInstanceOf(Function);
+    expect(reverseSortProp).toBeInstanceOf(Function);
+    expect(onSearchChangeProp).toBeInstanceOf(Function);
+    expect(setLoadingProp).toBeInstanceOf(Function);
+  });
 
-  test('', () => {
-    expect(1).toBe(1);
+  test('has access to redux store', () => {
+    const storeState = { starShip: initialState, loading: false };
+    const wrapper = setup(storeState);
+    const allStarShipsProp = wrapper.instance().props.allStarShips;
+    const filteredStarShipsProp = wrapper.instance().props.filteredStarShips;
+    const lastSortedProp = wrapper.instance().props.lastSorted;
+    const activeTableHeadersProp = wrapper.instance().props.activeTableHeaders;
+    const allTableHeadersProp = wrapper.instance().props.allTableHeaders;
+    const loadingProp = wrapper.instance().props.loading;
+    const distanceProp = wrapper.instance().props.distance;
+
+    expect(allStarShipsProp).toEqual(initialState.allStarShipData);
+    expect(filteredStarShipsProp).toEqual(initialState.filteredStarShipData);
+    expect(lastSortedProp).toBe(initialState.lastSorted);
+    expect(activeTableHeadersProp).toEqual(initialState.activeDataKeys);
+    expect(allTableHeadersProp).toEqual(initialState.allResponseKeys);
+    expect(loadingProp).toBe(false);
+    expect(distanceProp).toBe(initialState.distance);
+  });
+});
+
+describe('unconnected app', () => {
+  const onSearchChangeMock = jest.fn();
+  const onChangeTableHeadersMock = jest.fn();
+  const calcNumResuppliesMock = jest.fn();
+  const sortAlphabeticallyMock = jest.fn();
+
+  const props = {
+    allStarShips: sampleResponse,
+    filteredStarShips: sampleResponse,
+    lastSorted: '',
+    activeTableHeaders: ['name'],
+    allTableHeaders: ['name'],
+    loading: false,
+    distance: '',
+    makeApiCall: Function,
+    changeTableHeaders: onChangeTableHeadersMock,
+    calcNumResupplies: calcNumResuppliesMock,
+    sortAlphabetically: sortAlphabeticallyMock,
+    sortNummerically: Function,
+    sortConsumables: Function,
+    reverseSort: Function,
+    onSearchChange: onSearchChangeMock,
+    setLoading: Function
+  };
+
+  test('renders the page', () => {
+    const wrapper = shallow(<UnconnectedApp {...props} />);
+    const page = findByTestAttr(wrapper, 'page');
+    expect(page.length).toBe(1);
+  });
+
+  test('renders correct amount of rows', () => {
+    const wrapper = shallow(<UnconnectedApp {...props} />);
+    const tableRows = findByTestAttr(wrapper, 'table-results');
+    expect(tableRows.length).toBe(3);
+  });
+
+  test('on search change is called with correct value', () => {
+    const wrapper = shallow(<UnconnectedApp {...props} />);
+
+    const searchBar = findByTestAttr(wrapper, 'search-bar').dive();
+    const textInput = findByTestAttr(searchBar, 'text-input').dive();
+
+    textInput.simulate('change', { target: { value: '4000' } });
+
+    expect(onSearchChangeMock.mock.calls.length).toBe(1);
+    expect(onSearchChangeMock).toBeCalledWith('4000');
+  });
+
+  test('runs calcNumResupplies when search button is clicked', () => {
+    const wrapper = shallow(<UnconnectedApp {...props} />);
+
+    const searchBar = findByTestAttr(wrapper, 'search-bar').dive();
+    const searchBarButton = findByTestAttr(searchBar, 'search-button').dive();
+
+    searchBarButton.simulate('click');
+
+    expect(calcNumResuppliesMock.mock.calls.length).toBe(1);
+    expect(calcNumResuppliesMock).toBeCalledWith(
+      props.allStarShips,
+      props.distance,
+      props.activeTableHeaders
+    );
+  });
+
+  test('on checkbox change is called with correct value', () => {
+    const wrapper = shallow(<UnconnectedApp {...props} />);
+    const addRemoveFromTable = findByTestAttr(
+      wrapper,
+      'add-remove-from-table'
+    ).dive();
+
+    const dropdown = findByTestAttr(addRemoveFromTable, 'dropdown').dive();
+    const checkBox = findByTestAttr(dropdown, 'dropdown-info')
+      .children()
+      .first()
+      .dive()
+      .find('input');
+    checkBox.simulate('change', { target: { value: 'name' } });
+
+    expect(onChangeTableHeadersMock.mock.calls.length).toBe(1);
+    expect(onChangeTableHeadersMock).toBeCalledWith(
+      props.allStarShips,
+      props.activeTableHeaders,
+      'name'
+    );
+  });
+
+  test('sort by is run with correct header', () => {
+    const wrapper = shallow(<UnconnectedApp {...props} />);
+    const tableWrapper = findByTestAttr(wrapper, 'table-wrapper').dive();
+    const firstHeader = tableWrapper.find('th').first();
+    firstHeader.simulate('click');
+
+    expect(sortAlphabeticallyMock.mock.calls.length).toBe(1);
+    expect(sortAlphabeticallyMock).toBeCalledWith(
+      props.filteredStarShips,
+      'name'
+    );
+  });
+
+  test('loading text appears when loading prop is true', () => {
+    const setUpProps = { ...props, loading: true };
+    const wrapper = shallow(<UnconnectedApp {...setUpProps} />);
+
+    const loadingText = findByTestAttr(wrapper, 'loading-text');
+    expect(loadingText.length).toBe(1);
+  });
+
+  test('loading text doesnt appear when loading prop is false', () => {
+    const wrapper = shallow(<UnconnectedApp {...props} />);
+
+    const loadingText = findByTestAttr(wrapper, 'loading-text');
+    expect(loadingText.length).toBe(0);
   });
 });
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -24,6 +24,7 @@ import { StoreState } from './store/rootReducer';
 import { StarShipResponse, StarShipFiltered } from './interfaces/starship';
 
 interface IProps {
+  store?: any;
   allStarShips: StarShipResponse[];
   filteredStarShips: StarShipFiltered[];
   lastSorted: string;
@@ -42,7 +43,7 @@ interface IProps {
   setLoading: Function;
 }
 
-class App extends React.Component<IProps> {
+export class UnconnectedApp extends React.Component<IProps> {
   search = async () => {
     const { allStarShips, activeTableHeaders, distance } = this.props;
 
@@ -98,6 +99,7 @@ class App extends React.Component<IProps> {
     } = this.props;
     return (
       <Page
+        data-test="page"
         navHeader="Find the perfect Star Ship for your Journey"
         analysisLinks={['Star Ships']}
       >
@@ -112,21 +114,31 @@ class App extends React.Component<IProps> {
         {filteredStarShips.length > 0 ? (
           <>
             <AddRemoveFromTable
+              data-test="add-remove-from-table"
               tableHeaders={allTableHeaders}
               checkBoxActive={activeTableHeaders}
               onCheckboxChange={this.onCheckBoxChange}
             />
             <TableWrapper
+              data-test="table-wrapper"
               headers={filteredStarShips[0]}
               sortResult={this.sortBy}
             >
               {filteredStarShips.map((ship, index: number) => (
-                <TableRow key={index} data-test="results" rowData={ship} />
+                <TableRow
+                  key={index}
+                  data-test="table-results"
+                  rowData={ship}
+                />
               ))}
             </TableWrapper>
           </>
         ) : null}
-        {loading && <h4>Please wait while we reach out to the server!</h4>}
+        {loading && (
+          <h4 data-test="loading-text">
+            Please wait while we reach out to the server!
+          </h4>
+        )}
       </Page>
     );
   }
@@ -154,4 +166,4 @@ const mapDispatchToProps = {
   setLoading
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(UnconnectedApp);
