cffcb62ebe114219bf1e2bbc8ef8953993bf846a
Niall Quigley <niallquigley92@gmail.com>
2019-12-09 21:50:43 +0000

    Add home page

---
+++ client/src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Page, Heading } from '../../components';
+
+const Home = () => (
+  <Page
+    data-test="page"
+    navHeader="Welcome to the Star Wars Fan Page"
+    analysisLinks={['Star Ships']}
+  >
+    <Heading text="Click a link in the side bar to get started!" />
+  </Page>
+);
+export default Home;
--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from '../pages/Home';
 import StarShipAnalysis from '../pages/StarShipAnalysis';
 
 const App = () => (
   <Router>
     <Switch>
+      <Route exact path="/" component={Home} />
       <Route path="/starship/analysis" component={StarShipAnalysis} />
     </Switch>
   </Router>
