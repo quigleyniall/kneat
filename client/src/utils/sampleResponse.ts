1324f08e59d6976d0c8e2a23db96daa326a25fd6
Niall Quigley <niallquigley92@gmail.com>
2019-12-09 01:09:52 +0000

    Sample test response

---
+++ client/src/utils/sampleResponse.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/camelcase */
+export const sampleResponse = [
+  {
+    name: 'standard',
+    model: 'model 3',
+    manufacturer: 'tesla',
+    cost_in_credits: '30000',
+    length: '10',
+    max_atmosphering_speed: '200',
+    crew: '1',
+    passengers: '4',
+    cargo_capacity: '200',
+    consumables: '1500',
+    hyperdrive_rating: '5.0',
+    MGLT: '200',
+    starship_class: 'electric'
+  }
+];
