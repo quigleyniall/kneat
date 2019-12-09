75313a1e5db2e4b155fb13734280cd4f3dda6fa1
Niall Quigley <niallquigley92@gmail.com>
2019-12-09 18:02:01 +0000

    Remove unneeded algo

--- client/src/utils/sorting.ts
+++ client/src/utils/sorting.ts
@@ -1,16 +1,3 @@
-import { checkTimeToResupply } from './resupply';
-
-export const sortAlphabetically = (array: any[], objectKey: string) =>
-  array.sort((a, b) => {
-    if (a[objectKey].toLowerCase() < b[objectKey].toLowerCase()) {
-      return -1;
-    }
-    if (a[objectKey].toLowerCase() > b[objectKey].toLowerCase()) {
-      return 1;
-    }
-    return 0;
-  });
-
 export const sortNummerically = (array: any[], objectKey: string) =>
   array.sort((a, b) => {
     if (b[objectKey] === 'unknown' || b[objectKey] === 'n/a') {
@@ -19,19 +6,3 @@ export const sortNummerically = (array: any[], objectKey: string) =>
 
     return a[objectKey] - b[objectKey];
   });
-
-export const sortConsumables = (array: any[], objectKey: string) => {
-  array.sort((a, b) => {
-    if (b[objectKey] === 'unknown') {
-      return -1;
-    }
-
-    if (a[objectKey] === 'unknown') {
-      return 1;
-    }
-
-    return (
-      checkTimeToResupply(a[objectKey]) - checkTimeToResupply(b[objectKey])
-    );
-  });
-};
