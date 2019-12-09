a82cc92b6c736855e840016c6364fef5d5854c29
Niall Quigley <niallquigley92@gmail.com>
2019-12-09 14:42:43 +0000

    Update sample response

--- client/src/utils/sampleResponse.ts
+++ client/src/utils/sampleResponse.ts
@@ -1,18 +1,77 @@
 /* eslint-disable @typescript-eslint/camelcase */
 export const sampleResponse = [
   {
-    name: 'standard',
-    model: 'model 3',
-    manufacturer: 'tesla',
-    cost_in_credits: '30000',
-    length: '10',
-    max_atmosphering_speed: '200',
-    crew: '1',
-    passengers: '4',
-    cargo_capacity: '200',
-    consumables: '1500',
-    hyperdrive_rating: '5.0',
-    MGLT: '200',
-    starship_class: 'electric'
+    name: 'Millennium Falcon',
+    model: 'YT-1300 light freighter',
+    manufacturer: 'Corellian Engineering Corporation',
+    cost_in_credits: '100000',
+    length: '34.37',
+    max_atmosphering_speed: '1050',
+    crew: '4',
+    passengers: '6',
+    cargo_capacity: '100000',
+    consumables: '2 months',
+    hyperdrive_rating: '0.5',
+    MGLT: '75',
+    starship_class: 'Light freighter',
+    pilots: [
+      'https://swapi.co/api/people/13/',
+      'https://swapi.co/api/people/14/',
+      'https://swapi.co/api/people/25/',
+      'https://swapi.co/api/people/31/'
+    ],
+    films: [
+      'https://swapi.co/api/films/2/',
+      'https://swapi.co/api/films/7/',
+      'https://swapi.co/api/films/3/',
+      'https://swapi.co/api/films/1/'
+    ],
+    created: '2014-12-10T16:59:45.094000Z',
+    edited: '2014-12-22T17:35:44.464156Z',
+    url: 'https://swapi.co/api/starships/10/'
+  },
+  {
+    name: 'Y-wing',
+    model: 'BTL Y-wing',
+    manufacturer: 'Koensayr Manufacturing',
+    cost_in_credits: '134999',
+    length: '14',
+    max_atmosphering_speed: '1000km',
+    crew: '2',
+    passengers: '0',
+    cargo_capacity: '110',
+    consumables: '1 week',
+    hyperdrive_rating: '1.0',
+    MGLT: '80',
+    starship_class: 'assault starfighter',
+    pilots: [],
+    films: [
+      'https://swapi.co/api/films/2/',
+      'https://swapi.co/api/films/3/',
+      'https://swapi.co/api/films/1/'
+    ],
+    created: '2014-12-12T11:00:39.817000Z',
+    edited: '2014-12-22T17:35:44.479706Z',
+    url: 'https://swapi.co/api/starships/11/'
+  },
+  {
+    name: 'Rebel transport',
+    model: 'GR-75 medium transport',
+    manufacturer: 'Gallofree Yards, Inc.',
+    cost_in_credits: 'unknown',
+    length: '90',
+    max_atmosphering_speed: '650',
+    crew: '6',
+    passengers: '90',
+    cargo_capacity: '19000000',
+    consumables: '6 months',
+    hyperdrive_rating: '4.0',
+    MGLT: '20',
+    starship_class: 'Medium transport',
+    pilots: [],
+    films: ['https://swapi.co/api/films/2/', 'https://swapi.co/api/films/3/'],
+    created: '2014-12-15T12:34:52.264000Z',
+    edited: '2014-12-22T17:35:44.680838Z',
+    url: 'https://swapi.co/api/starships/17/'
   }
 ];
