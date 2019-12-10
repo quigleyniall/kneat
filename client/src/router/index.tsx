d91118973bc6308d4eb10a0854bc1101ac6eae05
Niall Quigley <niallquigley92@gmail.com>
2019-12-10 10:12:52 +0000

    Add new routes

--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -4,12 +4,20 @@ import Home from '../pages/Home';
 import StarShip from '../pages/StarShips';
 import StarShipAnalysis from '../pages/StarShipAnalysis';
 import history from './history';
+import Films from '../pages/Films';
+import Species from '../pages/Species';
+import People from '../pages/People';
+import Vehicle from '../pages/Vehicles';
 
 const App = () => (
   <Router history={history}>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/starship" component={StarShip} />
+      <Route path="/films" component={Films} />
+      <Route path="/species" component={Species} />
+      <Route path="/people" component={People} />
+      <Route path="/vehicles" component={Vehicle} />
       <Route path="/starship/analysis" component={StarShipAnalysis} />
     </Switch>
   </Router>
