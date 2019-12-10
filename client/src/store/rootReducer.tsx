98133753fb4cfc5182edb34c89d5978945f25bb5
Niall Quigley <niallquigley92@gmail.com>
2019-12-10 10:14:41 +0000

    Update root reducer

--- client/src/store/rootReducer.tsx
+++ client/src/store/rootReducer.tsx
@@ -4,17 +4,29 @@ import starShipAnalysis, {
 } from './reducers/starShipAnalysis';
 import starShip, { StarShip } from './reducers/starShip';
 import loading from './reducers/loading';
+import film, { Film } from './reducers/films';
+import species, { Species } from './reducers/species';
+import people, { People } from './reducers/people';
+import vehicle, { Vehicle } from './reducers/vehicle';
 
 export interface StoreState {
+  film: Film;
   starShipAnalysis: StarShipAnalysis;
   starShip: StarShip;
+  species: Species;
+  people: People;
+  vehicle: Vehicle;
   loading: boolean;
 }
 
 const rootReducer = combineReducers<StoreState>({
+  film,
   starShipAnalysis,
+  species,
   starShip,
-  loading
+  loading,
+  vehicle,
+  people
 });
 
 export default rootReducer;
