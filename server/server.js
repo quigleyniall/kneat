db6b9a9abc96daa26dfeac6144ef5c578406011a
Niall Quigley <niallquigley92@gmail.com>
2019-12-10 10:08:10 +0000

    Add Api calls

--- server/server.js
+++ server/server.js
@@ -29,6 +29,70 @@ api.get('/starships', async (req, res) => {
   return res.json(results);
 });
 
+api.get('/vehicles', async (req, res) => {
+  let page = 1;
+  let lastPage = false;
+  let results = [];
+
+  while (!lastPage) {
+    const request = await axios.get(`/vehicles?page=${page}`);
+    const response = await request.data;
+
+    results = [...results, ...response.results];
+
+    lastPage = response.next === null;
+
+    page++;
+  }
+
+  return res.json(results);
+});
+
+api.get('/species', async (req, res) => {
+  let page = 1;
+  let lastPage = false;
+  let results = [];
+
+  while (!lastPage) {
+    const request = await axios.get(`/species?page=${page}`);
+    const response = await request.data;
+
+    results = [...results, ...response.results];
+
+    lastPage = response.next === null;
+
+    page++;
+  }
+
+  return res.json(results);
+});
+
+api.get('/films', async (req, res) => {
+  const request = await axios.get(`/films`);
+  const response = await request.data;
+
+  return res.json(response.results);
+});
+
+api.get('/people', async (req, res) => {
+  let page = 1;
+  let lastPage = false;
+  let results = [];
+
+  while (!lastPage) {
+    const request = await axios.get(`/people?page=${page}`);
+    const response = await request.data;
+
+    results = [...results, ...response.results];
+
+    lastPage = response.next === null;
+
+    page++;
+  }
+
+  return res.json(results);
+});
+
 api.listen(8080, () => {
   console.log('Running');
 });
