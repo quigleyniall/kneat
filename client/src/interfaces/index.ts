674426866467d58a18910812361200a1c7d87375
Niall Quigley <niallquigley92@gmail.com>
2019-12-10 10:13:11 +0000

    Interfaces

---
+++ client/src/interfaces/index.ts
@@ -0,0 +1,124 @@
+export interface StarShipResponse {
+  name: string;
+  model: string;
+  manufacturer: string;
+  cost_in_credits: string;
+  length: string;
+  max_atmosphering_speed: string;
+  crew: string;
+  passengers: string;
+  cargo_capacity: string;
+  consumables: string;
+  hyperdrive_rating: string;
+  MGLT: number | string;
+  starship_class: string;
+}
+
+export interface StarShipFiltered {
+  name?: string;
+  model?: string;
+  manufacturer?: string;
+  cost_in_credits?: string;
+  length?: string;
+  max_atmosphering_speed?: string;
+  crew?: string;
+  passengers?: string;
+  cargo_capacity?: string;
+  consumables?: string;
+  hyperdrive_rating?: string;
+  MGLT?: number | string;
+  starship_class?: string;
+  number_of_resupplies?: string;
+}
+
+export interface FilmResponse {
+  director: string;
+  episode_id: number;
+  opening_crawl: string;
+  producer: string;
+  release_date: string;
+  title: string;
+}
+
+export interface FilmFiltered {
+  director?: string;
+  episode_id?: number;
+  opening_crawl?: string;
+  producer?: string;
+  release_date?: string;
+  title?: string;
+}
+
+export interface SpeciesResponse {
+  name: string;
+  classification: string;
+  designation: string;
+  average_height: string;
+  skin_colors: string;
+  hair_colors: string;
+  eye_colors: string;
+  average_lifespan: string;
+  language: string;
+}
+
+export interface SpeciesFiltered {
+  name?: string;
+  classification?: string;
+  designation?: string;
+  average_height?: string;
+  skin_colors?: string;
+  hair_colors?: string;
+  eye_colors?: string;
+  average_lifespan?: string;
+  language?: string;
+}
+
+export interface VehicleResponse {
+  name: string;
+  model: string;
+  manufacturer: string;
+  cost_in_credits: string;
+  length: string;
+  max_atmosphering_speed: string;
+  crew: string;
+  passengers: string;
+  cargo_capacity: string;
+  consumables: string;
+  vehicle_class: string;
+}
+
+export interface VehicleFiltered {
+  name?: string;
+  model?: string;
+  manufacturer?: string;
+  cost_in_credits?: string;
+  length?: string;
+  max_atmosphering_speed?: string;
+  crew?: string;
+  passengers?: string;
+  cargo_capacity?: string;
+  consumables?: string;
+  vehicle_class?: string;
+}
+
+export interface PeopleResponse {
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+}
+
+export interface PeopleFiltered {
+  name?: string;
+  height?: string;
+  mass?: string;
+  hair_color?: string;
+  skin_color?: string;
+  eye_color?: string;
+  birth_year?: string;
+  gender?: string;
+}
